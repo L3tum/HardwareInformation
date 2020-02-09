2fea99f324acd197eab9ba7c4aefb509bdf0de13
L3tum <9307432+L3tum@users.noreply.github.com>
2020-02-09 13:46:18 +0100

    Don't use MAP_PRIVATE on OSX

--- HardwareInformation/Opcode.cs
+++ HardwareInformation/Opcode.cs
@@ -102,12 +102,19 @@ namespace HardwareInformation
 
 			size = (ulong) (rdtscCode.Length + cpuidCode.Length);
 
-			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
 			{
 				// Unix   
 				codeBuffer = NativeMethods.mmap(IntPtr.Zero, size,
 					MmapProts.PROT_READ | MmapProts.PROT_WRITE | MmapProts.PROT_EXEC,
 					MmapFlags.MAP_ANON | MmapFlags.MAP_PRIVATE, -1, 0);
+			}
+			else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			{
+				// OSX   
+				codeBuffer = NativeMethods.mmap(IntPtr.Zero, size,
+					MmapProts.PROT_READ | MmapProts.PROT_WRITE | MmapProts.PROT_EXEC,
+					MmapFlags.MAP_ANON, -1, 0);
 			}
 			else
 			{
@@ -204,39 +211,38 @@ namespace HardwareInformation
 
 		[Map]
 		[Flags]
-		[CLSCompliant(false)]
-		internal enum MmapFlags : int
+		internal enum MmapFlags
 		{
-			MAP_SHARED = 0x01,     // Share changes.
-			MAP_PRIVATE = 0x02,     // Changes are private.
-			MAP_TYPE = 0x0f,     // Mask for type of mapping.
-			MAP_FIXED = 0x10,     // Interpret addr exactly.
+			MAP_SHARED = 0x01, // Share changes.
+			MAP_PRIVATE = 0x02, // Changes are private.
+			MAP_TYPE = 0x0f, // Mask for type of mapping.
+			MAP_FIXED = 0x10, // Interpret addr exactly.
 			MAP_FILE = 0,
-			MAP_ANONYMOUS = 0x20,     // Don't use a file.
+			MAP_ANONYMOUS = 0x20, // Don't use a file.
 			MAP_ANON = MAP_ANONYMOUS,
 
 			// These are Linux-specific.
-			MAP_GROWSDOWN = 0x00100,  // Stack-like segment.
-			MAP_DENYWRITE = 0x00800,  // ETXTBSY
-			MAP_EXECUTABLE = 0x01000,  // Mark it as an executable.
-			MAP_LOCKED = 0x02000,  // Lock the mapping.
-			MAP_NORESERVE = 0x04000,  // Don't check for reservations.
-			MAP_POPULATE = 0x08000,  // Populate (prefault) pagetables.
-			MAP_NONBLOCK = 0x10000,  // Do not block on IO.
+			MAP_GROWSDOWN = 0x00100, // Stack-like segment.
+			MAP_DENYWRITE = 0x00800, // ETXTBSY
+			MAP_EXECUTABLE = 0x01000, // Mark it as an executable.
+			MAP_LOCKED = 0x02000, // Lock the mapping.
+			MAP_NORESERVE = 0x04000, // Don't check for reservations.
+			MAP_POPULATE = 0x08000, // Populate (prefault) pagetables.
+			MAP_NONBLOCK = 0x10000 // Do not block on IO.
 		}
 
 		[Map]
 		[Flags]
-		[CLSCompliant(false)]
-		internal enum MmapProts : int
+		internal enum MmapProts
 		{
-			PROT_READ = 0x1,  // Page can be read.
-			PROT_WRITE = 0x2,  // Page can be written.
-			PROT_EXEC = 0x4,  // Page can be executed.
-			PROT_NONE = 0x0,  // Page can not be accessed.
+			PROT_READ = 0x1, // Page can be read.
+			PROT_WRITE = 0x2, // Page can be written.
+			PROT_EXEC = 0x4, // Page can be executed.
+			PROT_NONE = 0x0, // Page can not be accessed.
 			PROT_GROWSDOWN = 0x01000000, // Extend change to start of
+
 			//   growsdown vma (mprotect only).
-			PROT_GROWSUP = 0x02000000, // Extend change to start of
+			PROT_GROWSUP = 0x02000000 // Extend change to start of
 			//   growsup vma (mprotect only).
 		}
 
