eda561298ef85aaba00ddec32ef0777fcfe1df6e
L3tum <9307432+L3tum@users.noreply.github.com>
2020-10-22 11:03:26 +0200

    Fix: Support X86 Windows Environments in ThreadAffinity.cs Ref: #19

--- HardwareInformation/ThreadAffinity.cs
+++ HardwareInformation/ThreadAffinity.cs
@@ -9,119 +9,137 @@ using System.Runtime.InteropServices;
 
 namespace HardwareInformation
 {
-	internal static class ThreadAffinity
-	{
-		internal static ulong Set(ulong mask = 0xffffffffuL)
-		{
-			if (mask == 0)
-				return 0;
+    internal static class ThreadAffinity
+    {
+        internal static ulong Set(ulong mask = 0xffffffffuL)
+        {
+            if (mask == 0)
+            {
+                return 0;
+            }
+
+            var x86Mask = (uint) mask;
 
             var returnMask = 0xffffffffuL;
 
+            // Unix/POSIX
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-			{
-				// Unix/Posix
-				ulong result = 0;
-				if (NativeMethods.sched_getaffinity(0, (IntPtr) Marshal.SizeOf(result),
-					    ref result) != 0)
-					return 0;
-				if (NativeMethods.sched_setaffinity(0, (IntPtr) Marshal.SizeOf(mask),
-					    ref mask) != 0)
-					return 0;
-				return result;
-			} else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                ulong result = 0;
+                if (NativeMethods.sched_getaffinity(0, (IntPtr) Marshal.SizeOf(result),
+                    ref result) != 0)
+                {
+                    return 0;
+                }
+
+                if (NativeMethods.sched_setaffinity(0, (IntPtr) Marshal.SizeOf(mask),
+                    ref mask) != 0)
+                {
+                    return 0;
+                }
+
+                return result;
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
                 // OSX
                 return returnMask;
             }
-            
+
             // Windows
-			var threads = Process.GetCurrentProcess().Threads;
+            var threads = Process.GetCurrentProcess().Threads;
 
-			foreach (ProcessThread processThread in threads)
-			{
-				var threadId = NativeMethods.GetCurrentThreadId();
+            foreach (ProcessThread processThread in threads)
+            {
+                var threadId = NativeMethods.GetCurrentThreadId();
 
-				if (processThread.Id == threadId)
-				{
-					try
-					{
-						processThread.ProcessorAffinity = (IntPtr) mask;
-					}
-					catch (Win32Exception)
-					{
-						//Console.WriteLine("{0} with mask {1}", e.Message, GetIntBinaryString(mask));
-						// Intentionally left blank
-					}
-				}
-			}
+                if (processThread.Id == threadId)
+                {
+                    try
+                    {
+                        processThread.ProcessorAffinity = (IntPtr) (Environment.Is64BitProcess ? mask : x86Mask);
+                    }
+                    catch (Win32Exception)
+                    {
+                        //Console.WriteLine("{0} with mask {1}", e.Message, GetIntBinaryString(mask));
+                        // Intentionally left blank
+                    }
+                    catch (OverflowException)
+                    {
+                        // Intentionally left blank
+                    }
 
-			//Console.WriteLine("Mask worked {0}", GetIntBinaryString(mask));
+                    break;
+                }
+            }
 
-			return returnMask;
-		}
+            //Console.WriteLine("Mask worked {0}", GetIntBinaryString(mask));
 
-		private static string GetIntBinaryString(ulong n)
-		{
-			var b = new char[64];
-			var pos = 63;
-			var i = 0;
+            return returnMask;
+        }
 
-			while (i < 64)
-			{
-				if ((n & (1uL << i)) != 0)
-				{
-					b[pos] = '1';
-				}
-				else
-				{
-					b[pos] = '0';
-				}
+        private static string GetIntBinaryString(ulong n)
+        {
+            var b = new char[64];
+            var pos = 63;
+            var i = 0;
 
-				pos--;
-				i++;
-			}
+            while (i < 64)
+            {
+                if ((n & (1uL << i)) != 0)
+                {
+                    b[pos] = '1';
+                }
+                else
+                {
+                    b[pos] = '0';
+                }
 
-			return new string(b);
-		}
+                pos--;
+                i++;
+            }
 
-		private static class NativeMethods
-		{
-			private const string KERNEL = "kernel32.dll";
+            return new string(b);
+        }
 
-			private const string LIBC = "libc";
+        private static class NativeMethods
+        {
+            private const string KERNEL = "kernel32.dll";
 
-			[DllImport(KERNEL, CallingConvention = CallingConvention.Winapi)]
-			public static extern int GetCurrentThreadId();
+            private const string LIBC = "libc";
 
-			[DllImport(KERNEL, CallingConvention = CallingConvention.Winapi)]
-			public static extern int GetLastError();
+            [DllImport(KERNEL, CallingConvention = CallingConvention.Winapi)]
+            public static extern int GetCurrentThreadId();
 
-            /// <summary>
-            /// If pid is zero, then the calling thread is used.
-            /// </summary>
-            /// <param name="pid"></param>
-            /// <param name="maskSize"></param>
-            /// <param name="mask"></param>
-            /// <returns></returns>
-           [DllImport(LIBC)]
-			public static extern int sched_getaffinity(int pid, IntPtr maskSize,
-				ref ulong mask);
+            [DllImport(KERNEL, CallingConvention = CallingConvention.Winapi)]
+            public static extern int GetLastError();
 
             /// <summary>
-            /// If pid is zero, then the calling thread is used.
+            ///     If pid is zero, then the calling thread is used.
             /// </summary>
             /// <param name="pid"></param>
             /// <param name="maskSize"></param>
             /// <param name="mask"></param>
             /// <returns></returns>
             [DllImport(LIBC)]
-			public static extern int sched_setaffinity(int pid, IntPtr maskSize,
-				ref ulong mask);
+            public static extern int sched_getaffinity(int pid, IntPtr maskSize,
+                ref ulong mask);
+
+            /// <summary>
+            ///     If pid is zero, then the calling thread is used.
+            /// </summary>
+            /// <param name="pid"></param>
+            /// <param name="maskSize"></param>
+            /// <param name="mask"></param>
+            /// <returns></returns>
+            [DllImport(LIBC)]
+            public static extern int sched_setaffinity(int pid, IntPtr maskSize,
+                ref ulong mask);
 
             [DllImport(LIBC, CharSet = CharSet.Unicode)]
-            public static extern int sysctlbyname(string function, ref Int32 coreCount, ref Int32 length, int newP = 0, int newPLength = 0);
-
+            public static extern int sysctlbyname(string function, ref int coreCount, ref int length, int newP = 0,
+                int newPLength = 0);
         }
-	}
+    }
 }
